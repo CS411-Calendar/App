1e3a1387a062a69a4064126fd11ac8fedefdf1a2
Curtis Mason <cumason@bu.edu>
2021-03-19 16:02:25 -0400

    basic form

--- src/onboarding/Landing.tsx
+++ src/onboarding/Landing.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function Landing() {
   return <div>Landing Page</div>
--- src/onboarding/registration/UserCredentials.tsx
+++ src/onboarding/registration/UserCredentials.tsx
@@ -2,7 +2,36 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function UserCredentials() {
-  return <div>UserCredentials</div>
+  // const username_validation = /^(?=[a-zA-Z0-9._]{3,20}$)(?!.*[_.]{2})/
+  // const password_validation = //
+
+  return (
+    <div>
+      <form>
+        <input
+          name="username"
+          type="text"
+          placeholder="input username"
+          required
+        />
+        <input name="email" type="text" placeholder="input email" required />
+        <input name="email" type="text" placeholder="input email" required />
+
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+        />
+        <input
+          name="confirm_password"
+          type="password"
+          placeholder="confirm password"
+          required
+        />
+      </form>
+    </div>
+  )
 }
 
 export default UserCredentials
