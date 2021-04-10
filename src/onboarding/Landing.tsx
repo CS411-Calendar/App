948b4d9c813945ae6121729fac0f12151620554e
kimonazj <kimonazj@users.noreply.github.com>
2021-04-10 08:32:44 +0800

    finish landing page

--- src/onboarding/Landing.tsx
+++ src/onboarding/Landing.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "tailwindcss/tailwind.css";
+import Feed from "../feed";
+import { Link } from "react-router-dom";
+// import styles from "Landing.css"
 
 function Landing() {
-  return <div>Landing Page</div>
+  
+  return (
+    <div className="grid grid-rows-2 item-center justify-items-center h-screen">
+      <div className="flex justify-items-center items-center h-full">
+        <h1 className="text-6xl self-center font-bold ">
+          {" "}
+          Wlecome to MY CALENDAR{" "}
+        </h1>
+      </div>
+      <div>
+        <Link to="/feed">
+          <div
+            className="border-2 px-8 py-4 bg-gray-300 rounded-lg items-center"
+            id="login"
+          >
+            Login with Google
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Landing
+export default Landing;
