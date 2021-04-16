7c07f4509de3fbd16ce1e0f598d35025eee8df3c
cefeng9988 <cefeng@bu.edu>
2021-04-15 20:23:11 -0400

    Exact path added

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
         <Route exact path="/sign-in">
           <LogIn />
         </Route>
-        <Route exact path="/feed">
+        <Route exact  path="/feed">
           <Feed/>
         </Route>
 
