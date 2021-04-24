09a52917e25bcb933a720bfaf0ab21c7f820c330
Curtis Mason <cumason@bu.edu>
2021-04-23 20:19:12 -0400

    put landing page on root

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/landing">
+        <Route path="/">
           <Landing />
         </Route>
         <Route path="/sign-up">
