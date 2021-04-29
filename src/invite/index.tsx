cdca0d53142cee7d5b6f9f2b9c1771f107502c8f
Takamitsu Shirono <takshiro@bu.edu>
2021-04-28 23:01:11 -0400

    started working on invite page

--- src/invite/index.tsx
+++ src/invite/index.tsx
@@ -1,13 +1,42 @@
-import React,{useEffect, useState} from 'react'
-import { useHistory, useParams } from 'react-router-dom'
-type ParamsType = {id:string}
-export default function Invite() {
+import React, { useEffect, useState } from "react"
+import { useHistory, useParams } from "react-router-dom"
+import Calendar from "@ericz1803/react-google-calendar"
+import apiGoogleconfig from "../config/apiGoogleconfig.json"
 
-    const params = useParams<ParamsType>()
-    console.log(params)
-    return (
-        <div>
-            {params.id}
-        </div>
-    )
+type ParamsType = { id: string }
+export default function Invite() {
+  // params stuff
+  const params = useParams<ParamsType>()
+  console.log(params)
+
+  // info to display calendar
+  const API_KEY = apiGoogleconfig.apiKey
+  let calendars = [
+    {
+      calendarId: "u.s.apple1102@gmail.com",
+      color: "#B241D1", //optional, specify color of calendar 2 events
+    },
+  ]
+
+  return (
+    <div>
+      <div>
+        {/* Potentially add more attributes to change starting month / date? */}
+        <Calendar apiKey={API_KEY} calendars={calendars} />
+      </div>
+      <div>
+        The ID of this page is: <b>{params.id}</b>
+      </div>
+      <div className="flex flex-col h-screen my-auto items-center bgimg bg-cover">
+        <button
+          onClick={(e) => {
+            console.log("Join Calendar")
+          }}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Join Calendar
+        </button>
+      </div>
+    </div>
+  )
 }
