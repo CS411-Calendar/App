445f17af20bab13db30dc8e92d903cd4d66eae41
Curtis Mason <cumason@bu.edu>
2021-05-01 19:51:21 -0400

    better loading screen

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -1,5 +1,6 @@
 export const Loading = () => (
   <div className="flex flex-col h-screen items-center justify-center">
-    <div>Loading...</div>
+    <div className="font-extrabold text-xl, py-2">Loading...</div>
+    <p>Make sure your google calendar is set to public.</p>
   </div>
 )
