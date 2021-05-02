51a2e2671a848244684384759c310d9c18fba292
cefeng9988 <cefeng@bu.edu>
2021-05-02 10:14:12 -0400

    Invite path: create group event button and modal

--- src/invite/index.tsx
+++ src/invite/index.tsx
@@ -3,18 +3,50 @@ import { useParams } from 'react-router-dom'
 import Calendar from '@ericz1803/react-google-calendar'
 import apiGoogleconfig from '../config/apiGoogleconfig.json'
 import { oauthSetup, isAuthorized, login, getEmail } from '../lib/auth'
+import {UserIcon} from "@heroicons/react/solid";
+import {CreateModal} from "../components/modal";
+import {createSingleEvent} from "../lib/event";
+import {WeatherRes} from "../feed";
+import {API_URL} from "../constants";
+
+type WeatherArgs = {
+  latitude: number
+  longitude: number
+}
 
 type ParamsType = { id: string }
 export default function Invite() {
+  const [weather, setWeather] = useState<WeatherRes[] | undefined>()
+  const [showCreateModal, setShowCreateModal] = useState(false)
+  const [showAlert, setShowAlert] = useState(false)
+
   useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) =>
+        getWeather(position.coords),
+    )
     oauthSetup()
   }, [])
+
+  const getWeather = async (args: WeatherArgs) => {
+    try {
+      const res = await fetch(
+          `${API_URL}/weather?lat=${args.latitude}&long=${args.longitude}`,
+      )
+      if (res.status === 200) {
+        const data: WeatherRes[] = await res.json()
+        setWeather(data)
+        console.log('Weather Server: ', weather)
+      }
+    } catch (e) {
+      console.error('Server unreachable')
+    }
+  }
 
   const [acceptedInvite, setAcceptedInvite] = useState(false)
   const acceptInviteClick = async () => {
     if (isAuthorized()) {
       // Extract user email
-      console.log(getEmail())
+      console.log("User email: ", getEmail())
     } else {
       await login()
       // Extract user email
@@ -36,34 +68,107 @@ export default function Invite() {
     },
   ]
 
+  function createvent(e) {
+    setShowAlert(false)
+    setShowCreateModal(true)
+  }
+
+  function handleCreateSubmit(e) {
+    e.preventDefault()
+
+    let startdate = e.target.elements.startdate?.value
+    let enddate = e.target.elements.enddate?.value
+    let starttime = e.target.elements.starttime?.value
+    let endtime = e.target.elements.endtime?.value
+    let event_name = e.target.elements.event_name?.value
+    let event_location = e.target.elements.event_location?.value
+
+    if (startdate === '' || enddate === '') {
+      setShowAlert(true)
+    } else if (startdate > enddate) {
+      setShowAlert(true)
+    } else if (startdate === enddate && starttime > endtime) {
+      setShowAlert(true)
+    } else {
+      //example output: 2021-04-16 10:24 2021-04-16 22:29 study 411 CAS344
+      if (event_name === '') {
+        event_name = 'New Event'
+      }
+      if (event_location === '') {
+        event_location = 'Home'
+      }
+
+      createSingleEvent(
+          startdate,
+          starttime,
+          enddate,
+          endtime,
+          event_name,
+          event_location,
+      )
+
+      setShowCreateModal(false)
+    }
+  }
+
   return (
     <div>
       <div>
         {/* Potentially add more attributes to change starting month / date? */}
         <Calendar apiKey={API_KEY} calendars={calendars} />
       </div>
+      <div className="fixed left-0 top-1/2">
+        <button
+            className="bg-gray-300 py-2 px-2 rounded border-2 grid"
+            onClick={createvent}
+        >
+          <UserIcon className="text-blue-500 h-7 w-7 justify-self-center"></UserIcon>
+          <span className="text-xs">Create Group Event</span>
+        </button>
+        {showCreateModal ? (
+            <CreateModal
+                {...{
+                  handleCreateSubmit,
+                  setShowAlert,
+                  setShowCreateModal,
+                  showAlert,
+                  weather,
+                }}
+            />
+        ) : null}
+      </div>
       <div>
         The ID of this page is: <b>{params.id}</b>
       </div>
       <div className="flex flex-col h-screen my-auto items-center bgimg bg-cover">
         {!acceptedInvite && (
-          <button
-            onClick={(e) => {
-              acceptInviteClick()
-            }}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Join Calendar
-          </button>
+          <>
+            <div className={"fixed bottom-20 bg-gray-300 text-black py-2 px-5 rounded"}>
+              The event will occur between May 5, 2021 to June 10, 2021.
+            </div>
+            <button
+              onClick={(e) => {
+                acceptInviteClick()
+              }}
+              className="fixed bottom-5 hover:bg-blue-100 hover:text-black bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Share Calendar
+            </button>
+          </>
         )}
         {acceptedInvite && (
-          <h1>
-            {' '}
-            Thank you for accepting the invite from&nbsp;
-            {/* change this part to show the host */}
-            {calendars[0].calendarId}. The owner will send you a calendar invite
-            via E-mail shortly once all attendees accept the invite.
-          </h1>
+          <>
+            <div className={"fixed bottom-20 bg-gray-300 text-black py-2 px-5 rounded"}>
+              The event will occur between May 5, 2021 to June 10, 2021.
+            </div>
+            <h1 className={"fixed bottom-5 bg-white"}>
+              {' '}
+              Thank you for accepting the invite from&nbsp;
+              {/* change this part to show the host */}
+              {calendars[0].calendarId}. The owner will send you a calendar invite
+              via E-mail shortly once all attendees accept the invite.
+            </h1>
+          </>
         )}
       </div>
     </div>
